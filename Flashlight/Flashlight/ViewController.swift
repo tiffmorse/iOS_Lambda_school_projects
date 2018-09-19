bc2c3f277ac110cc800a744d7822c532945ba8d1
tiffmorse <tiffmorse@mac.com>
2018-09-19 08:10:32 -0400

    added Flashlight and Rainbow projects

---
+++ Flashlight/Flashlight/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Flashlight
+//
+//  Created by Tiffany on 9/15/18.
+//  Copyright © 2018 Tiffany. All rights reserved.
+//  first project in lambda school class
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBAction func turnFlashlightOn(_ sender: Any) {
+        view.backgroundColor = .red
+    }
+    
+
+    @IBAction func turnFlashlightOff(_ sender: Any) {
+        view.backgroundColor = .blue
+    }
+    
+}
+
---
+++ Rainbow/Rainbow/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Rainbow
+//
+//  Created by Tiffany on 9/15/18.
+//  Copyright © 2018 Tiffany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func red(_ sender: Any) {
+        view.backgroundColor = .red
+    }
+    
+    @IBAction func orange(_ sender: Any) {
+        view.backgroundColor = .orange
+    }
+    
+    @IBAction func yellow(_ sender: Any) {
+        view.backgroundColor = .yellow
+    }
+    
+    @IBAction func green(_ sender: Any) {
+        view.backgroundColor = .green
+    }
+    
+    @IBAction func blue(_ sender: Any) {
+        view.backgroundColor = .blue
+    }
+    
+    @IBAction func violet(_ sender: Any) {
+        view.backgroundColor = .purple
+    }
+
+}
+
