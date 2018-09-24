db29045164569d0d6f37597d7d0140e0e0f59b21
tiffmorse <tiffmorse@mac.com>
2018-09-24 18:59:17 -0400

    added time check to flashlight

--- Flashlight/Flashlight/ViewController.swift
+++ Flashlight/Flashlight/ViewController.swift
@@ -8,14 +8,23 @@
 
 import UIKit
 
+let calendar = Calendar(identifier: .gregorian)
+let currentDate = Date()
+let hour = calendar.component(.hour, from: currentDate)
+//above code pasted from homework instructions for conditionals and optionals
+
 class ViewController: UIViewController {
 
-    
     @IBAction func turnFlashlightOn(_ sender: Any) {
-        view.backgroundColor = .red
+        if hour >= 6 && hour <= 22 {
+            view.backgroundColor = .yellow
+        } else {
+        view.backgroundColor = .gray
     }
+            print(hour)
+}
     
-
+    
     @IBAction func turnFlashlightOff(_ sender: Any) {
         view.backgroundColor = .blue
     }
